0e304ea18040fddb96ff8696c278baf45c949a5a
Christian Groll <groll.christian.edu@gmail.com>
2014-02-07 10:35:14 +0100

    main types added with tests

--- src/AssetMgmt.jl
+++ src/AssetMgmt.jl
@@ -1,5 +1,21 @@
 module AssetMgmt
 
-# package code goes here
+using TimeData
+using DataFrames
+
+##############################################################################
+##
+## Exported methods and types
+##
+##############################################################################
+
+export # functions and types
+Investments,
+Portfolio
+
+include("constraints.jl")
+include("investments.jl")
+include("portfolio.jl")
+include("moments.jl")
 
 end # module
