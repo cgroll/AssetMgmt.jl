4fd0ab2831051b20762783296a4f995992d127b7
Christian Groll <groll.christian.edu@gmail.com>
2014-04-30 17:32:10 +0200

    some code on iterators and grouping of data

--- app/asset_mgmt_analysis.jl
+++ app/asset_mgmt_analysis.jl
@@ -5,6 +5,35 @@ include("/home/chris/.julia/v0.3/Gadfly/src/Gadfly.jl")
 include("/home/chris/.julia/v0.3/AssetMgmt/src/AssetMgmt.jl")
 ## using DateTime
 
+##########################################
+## define dictionary inverting function ##
+##########################################
+
+function invertDict(dict::Dict)
+    invDict = Dict()
+    for val in unique(values(dict))
+        ## for each possible value, find all keys
+        ## @show val
+        keysWithGivenValue = Array(Symbol, 0)
+        ## counter = 1
+        for key in dict
+            ## @show key
+            if key[2] == val
+                push!(keysWithGivenValue, key[1])
+                ## counter = counter + 1
+            end
+        end
+        invDict[val] = keysWithGivenValue
+    end
+    return invDict
+end
+
+kk = by(sectors, :sector, x -> transpose(x[:, 1]))
+
+for ii=1:size(kk, 1)
+    kk[ii, 2] = array(kk[ii, 2])
+end
+
 ##############
 ## get data ##
 ##############
@@ -12,6 +41,84 @@ include("/home/chris/.julia/v0.3/AssetMgmt/src/AssetMgmt.jl")
 include("/home/chris/research/julia/EconDatasets/src/EconDatasets.jl")
 logRet = EconDatasets.dataset("SP500")
 sectors = EconDatasets.dataset("Sectors")
+
+## transform sector names into symbols
+sectorSymbols = [symbol(symb) for symb in sectors[:, 1]]
+sectors[:symbol] = sectorSymbols
+
+## store asset / sector pairs as dictionary
+sectDict = {sectors[i, 1] => sectors[i,2] for i=1:size(sectors, 1)}
+
+## find number of stocks per sector
+by(sectors, :sector, x -> size(x, 1))
+
+## invert sector dictionary
+assetsInSector = invertDict(sectDict)
+
+## get number of sectors
+nSectors = length(assetsInSector)
+
+## get mean sector returns
+sectorRetsDf = DataFrame()
+for sect in assetsInSector
+    ## @show sect
+    sectorData = logRet[sect[2]]
+    sectorMean = rowmeans(sectorData)
+    sectorRetsDf[convert(Symbol, sect[1])] = core(sectorMean)[:]
+end
+
+sectorRets = Timematr(sectorRetsDf, idx(logRet))
+
+plot(x=[1:size(sectorRets, 1)], y=core(sectorRets)[:, 1])
+
+k = plot(x=rand(10), y=rand(10), Geom.line)
+
+k = AssetMgmt.plot(sectorRets)
+
+draw(PDF("myplot.pdf", 6inch, 3inch), k)
+
+energyRets = logRet[assetsInSector["Energy"]]
+
+symbs = convert(Array{Symbol, 1}, sectors[:, 1][:])
+
+
+
+for stock in sectDict
+    @show stock[2]
+end
+
+
+sectTuple = [(sectors[ii, 1], sectors[ii, 2]) 
+             for ii=1:size(sectors, 1) ]
+
+kk = groupby(sectTuple, x -> x[2])
+
+for stock in sectDict
+    @show stock[2]
+end
+
+kk = groupby(sectDict, x -> x[2])
+
+collect(groupby(["face", "foo", "bar", "book", "baz", "zzz"], x -> x[1]))
+
+for ii in kk
+    @show ii
+end
+
+for vals in values(sectDict)
+    @show vals
+end
+
+dictSymbs = convert(Dict, sectors)
+groupby(dictSymbs, dictSymbs)
+    
+smallData = logRet[1:10, :]
+
+
+
+for ii=1:size(smallData, 1)
+    
+
 
 ###################################
 ## transform to discrete returns ##
