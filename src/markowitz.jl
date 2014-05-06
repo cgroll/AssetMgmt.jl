115302c8f0c9684f6ebaeb85768e80248d48a3c6
Christian Groll <groll.christian.edu@gmail.com>
2014-05-06 12:07:26 +0200

    moment based strategies proceeded

--- src/markowitz.jl
+++ src/markowitz.jl
@@ -9,3 +9,14 @@ function gmv(mus::DataFrame, covMatr::DataFrame)
     return wgts'
 end
     
+function gmv(mus::Array{Float64, 2}, covMatr::Array{Float64, 2})
+    ## global minimum variance portfolio
+    nAss = size(mus, 2)
+
+    oneArr = ones(nAss)
+    invCov = inv(covMatr)
+    wgts = 1./(oneArr'*invCov*oneArr).*invCov*oneArr
+
+    return wgts'
+end
+    
--- src/mu_sigma_estimation.jl
+++ src/mu_sigma_estimation.jl
@@ -1,14 +1,25 @@
 function empiricalEstimator(discRet::Array{Float64, 2})
     ## for given data, apply estimator
 
-    mus = mean(discRet)
+    mus = mean(discRet, 1)
     covMatr = cov(discRet)
 
     return (mus, covMatr)
 end
 
+function subsamples(nObs, minObs; rollingWindow=false)
+    for ii=minObs:(nObs-1)
+        if rollingWindow
+            produce((ii-minObs+1, ii))
+        else
+            produce((1, ii))
+        end
+    end
+end
+
 function applyEstimator(discRet::Timematr, estFunc::Function,
-                        minObs::Int; rollingWindow=false)
+                        responseFunc::Function,
+                        minObs::Int; auxData=(), rollingWindow=false)
     ## apply estimator to data
     ##
     ## Inputs:
@@ -20,37 +31,62 @@ function applyEstimator(discRet::Timematr, estFunc::Function,
     ## 	rollingWindow	optional: default value is false, so that
     ## 						estimation procedure makes use of complete
     ## 						data sample
-    
 
-    nObs = size(discRet, 2)
-    for ii=minObs:nObs
-        if rollingWindow
-            produce(estFunc(discRet[(ii-minObs+1):ii, :]), idx(discRet)[ii])
-        else
-            produce(estFunc(discRet[1:ii, :]), idx(discRet)[ii])
-        end
+    valsDf = DataFrame(size(discRet)...)
+    rename!(valsDf, names(valsDf), names(discRet))
+    invs = Timedata(valsDf, idx(discRet))
+        
+    nObs = size(discRet, 1)
+    t = Task(() -> subsamples(nObs, minObs, rollingWindow=rollingWindow))
+
+    ## get data as array
+    discRetArr = core(discRet)
+
+    auxDataArr = ()
+    for ii=1:length(auxData)
+        auxDataArr = tuple(auxDataArr..., core(auxData[ii]))
     end
+
+    for x in t
+        indices = x
+        println(indices)
+
+        ## get auxilliary data
+        getAux = x -> getindex(x, indices[1]:indices[2],
+                               1:size(x,2))
+        outp = ()
+        for ii=1:length(auxData)
+            outp = tuple(outp..., getAux(auxDataArr[ii]))
+        end
+        (mus, covMatr) = estFunc(discRetArr[indices[1]:indices[2], :],
+                                 outp...)
+
+        ## get weights
+        wgts = responseFunc(mus, covMatr)
+
+        ## capture calculated weights
+        invs.vals[(indices[2]+1), :] = wgts
+        
+    end
+    return invs
 end
 
-Task(() -> applyEstimator(discRet, estFunc, minObs,
-                          rollingWindow))
+estFunc = empiricalEstimator
+responseFunc = AssetMgmt.gmv
+applyEstimator(discRet, estFunc, responseFunc, 1500)
+
 function getMarkowitzWgts(t::Task)
 
     ## get number of wgts and preallocate storage for Array{Float64, 2}
-    
     for x in t
-        
         ## get mu / sigma values
         (mus, covMatr) = consume(x)
-
         ## get associated weights
         wgts = gmv(mus, covMatr)
-
-        ## 
-
     end
 
     ## transform Array{Float64, 2} to Timematr, filling values from
     ## end
-
 end
+
+
