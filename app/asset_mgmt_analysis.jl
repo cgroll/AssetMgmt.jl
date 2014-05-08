a05b8d5ee8fe0ae492e268f6e54760128626ba99
Christian Groll <groll.christian.edu@gmail.com>
2014-05-08 12:21:48 +0200

    gmv analysis proceeded

--- app/asset_mgmt_analysis.jl
+++ app/asset_mgmt_analysis.jl
@@ -1,3 +1,7 @@
+###################
+## load packages ##
+###################
+
 using TimeData
 ## using Plotly
 ## using Gadfly
@@ -5,11 +9,20 @@ include("/home/chris/.julia/v0.3/Gadfly/src/Gadfly.jl")
 include("/home/chris/.julia/v0.3/AssetMgmt/src/AssetMgmt.jl")
 ## using DateTime
 
+########################
+## specify parameters ##
+########################
 
-## kk = by(sectors, :sector, x -> transpose(x[:, 1]))
-## for ii=1:size(kk, 1)
-##     kk[ii, 2] = array(kk[ii, 2])
-## end
+## choose starting point to pick subsample, maximum 4607
+cutoff = 4000
+
+## choose mu-sigma-estimator and parameters
+muSigmaEstimator = AssetMgmt.empiricalEstimator
+minObs = 100
+rollingWindow = false
+
+## choose response strategy
+responseFunc = AssetMgmt.gmv
 
 ##############
 ## get data ##
@@ -18,15 +31,35 @@ include("/home/chris/.julia/v0.3/AssetMgmt/src/AssetMgmt.jl")
 include("/home/chris/research/julia/EconDatasets/src/EconDatasets.jl")
 logRet = EconDatasets.dataset("SP500")
 sectorsStr = EconDatasets.dataset("Sectors")
+## interest rates
 
 ## transform to discrete non-percentage returns
-discRet = exp(logRet/100).-1
+discRetAll = exp(logRet/100).-1
+discRet = discRetAll[cutoff:end, :]
 
-t = Task(() -> AssetMgmt.produceEmpiricalMoments(discRet, 4400))
+(nObs, nAss) = size(discRet)
+
+t = Task(() -> AssetMgmt.produceMoments(discRet,
+                                        muSigmaEstimator,
+                                        (),
+                                        minObs=minObs,
+                                        rolling=rollingWindow))
+
+############################
+## get associated weights ##
+############################
+
+wgts = NaN*ones(size(discRet))
+dates = rep(false, nObs)
 for x in t
-    println(x)
+    (mus, covMatr, index) = x
+    println(index)
+    dates[index] = true
+    wgts[index, :] = responseFunc(mus, covMatr)
 end
 
+wgtsDf = AssetMgmt.composeDataFrame(wgts[dates, :], names(discRet))
+invs = AssetMgmt.Investments(wgtsDf, idx(discRet)[dates])
 
 
 ## transform sector entries into symbols
@@ -37,7 +70,7 @@ sectors[:ticker] = tickerSymbols
 sectors[:sector] = sectorSymbols
 
 ## transform dataframe to dictionary
-(nObs, nAss) = size(logRet)
+
 sectDict = {sectors[ii, 1] => sectors[ii, 2] for ii=1:nAss}
 
 ##########################################
--- src/mu_sigma_estimation.jl
+++ src/mu_sigma_estimation.jl
@@ -1,6 +1,21 @@
-function produceEmpiricalMoments(discRet::Timematr, minObs;
-                                 rolling=false)
+## general interface:
+## - specify minimum size of sample and whether rolling window should
+##   be applied
+## - specify function to estimate and auxilliary data required for
+##   estimator
+##
+## each estimator function signature should be:
+## (discRet, begInd, endInd, auxData)
 
+#######################
+## general interface ##
+#######################
+
+function produceMoments(discRet::Timematr,
+                        estimator::Function,
+                        auxData::Tuple;
+                        minObs=200, rolling=false)
+    ## general interface to moments estimation
     nObs = size(discRet, 1)
 
     ## specify how subsamples should be created
@@ -11,91 +26,30 @@ function produceEmpiricalMoments(discRet::Timematr, minObs;
 
     for subsample in produceSubsamples
         indices = subsample
-        (mu, covMatr) = AssetMgmt.empiricalEstimator(dataArr)
+        (mu, covMatr) = estimator(dataArr, indices..., auxData...)
         nextDay = indices[2]+1
         produce(mu, covMatr, nextDay)
     end
 end
-    
-function empiricalEstimator(discRet::Array{Float64, 2})
+
+#######################
+## empirical moments ##
+#######################
+
+function empiricalEstimator(discRet::Array{Float64, 2},
+                            begInd::Int,
+                            endInd::Int)
     ## for given data, apply estimator
 
-    mus = mean(discRet, 1)
-    covMatr = cov(discRet)
+    mus = mean(discRet[begInd:endInd, :], 1)
+    covMatr = cov(discRet[begInd:endInd, :])
 
     return (mus, covMatr)
 end
 
-## function applyEstimator(discRet::Timematr, estFunc::Function,
-##                         responseFunc::Function,
-##                         minObs::Int; auxData=(), rollingWindow=false)
-##     ## apply estimator to data
-##     ##
-##     ## Inputs:
-##     ## 	discRet			Array{Float64, 2} of discrete returns
-##     ## 	estFunc 			function with Array{Float64, 2} discrete
-##     ## 						returns as only input
-##     ## 	minObs			minimum number of observations required for
-##     ## 						estimation procedure
-##     ## 	rollingWindow	optional: default value is false, so that
-##     ## 						estimation procedure makes use of complete
-##     ## 						data sample
+###################
+## factor models ##
+###################
 
-##     valsDf = DataFrame(size(discRet)...)
-##     rename!(valsDf, names(valsDf), names(discRet))
-##     invs = Timedata(valsDf, idx(discRet))
-        
-##     nObs = size(discRet, 1)
-##     t = Task(() -> subsamples(nObs, minObs, rollingWindow=rollingWindow))
-
-##     ## get data as array
-##     discRetArr = core(discRet)
-
-##     auxDataArr = ()
-##     for ii=1:length(auxData)
-##         auxDataArr = tuple(auxDataArr..., core(auxData[ii]))
-##     end
-
-##     for x in t
-##         indices = x
-##         println(indices)
-
-##         ## get auxilliary data
-##         getAux = x -> getindex(x, indices[1]:indices[2],
-##                                1:size(x,2))
-##         outp = ()
-##         for ii=1:length(auxData)
-##             outp = tuple(outp..., getAux(auxDataArr[ii]))
-##         end
-##         (mus, covMatr) = estFunc(discRetArr[indices[1]:indices[2], :],
-##                                  outp...)
-
-##         ## get weights
-##         wgts = responseFunc(mus, covMatr)
-
-##         ## capture calculated weights
-##         invs.vals[(indices[2]+1), :] = wgts
-        
-##     end
-##     return invs
-## end
-
-## estFunc = empiricalEstimator
-## responseFunc = AssetMgmt.gmv
-## applyEstimator(discRet, estFunc, responseFunc, 1500)
-
-## function getMarkowitzWgts(t::Task)
-
-##     ## get number of wgts and preallocate storage for Array{Float64, 2}
-##     for x in t
-##         ## get mu / sigma values
-##         (mus, covMatr) = consume(x)
-##         ## get associated weights
-##         wgts = gmv(mus, covMatr)
-##     end
-
-##     ## transform Array{Float64, 2} to Timematr, filling values from
-##     ## end
-## end
 
 
