ef51b21dc2cdb91b68b0b48677f01734debf0472
cgroll <groll.christian.edu@gmail.com>
2015-12-10 17:57:05 +0100

    moving window sample moments added

--- src/universeModels.jl
+++ src/universeModels.jl
@@ -31,6 +31,32 @@ function SampleMoments()
                          Array(Symbol, 0))
 end
 
+## Moving window sample moments
+##-----------------------------
+
+type MovWinSampleMoments <: MuSigmaModel
+    mu::Array{Float64, 1}
+    sigma::Array{Float64, 2}
+    names::Array{Symbol, 1}
+
+    function MovWinSampleMoments(mu::Array{Float64, 1},
+                                 sigma::Array{Float64, 2},
+                                 names::Array{Symbol, 1})
+        ## TODO: check for equal sizes
+
+        return new(mu, sigma, names)
+    end
+end
+
+## define empty constructor
+##-------------------------
+
+function MovWinSampleMoments()
+    return MovWinSampleMoments(Array(Float64, 0),
+                               Array(Float64, 0, 0),
+                               Array(Symbol, 0))
+end
+
 
 ## ExpWeighted
 ##------------
@@ -100,10 +126,15 @@ function display(mod::MuSigmaModel)
     end
 end
 
-## plotting
-##---------
+##################
+## extract info ##
+##################
 
-
+function getVolas(mod::MuSigmaModel)
+    nAss = size(mod.sigma, 1)
+    variances = Float64[mod.sigma[ii, ii] for ii=1:nAss]
+    return sqrt(variances)
+end
 
 ################
 ## estimators ##
@@ -128,6 +159,27 @@ function fitModel(::Type{SampleMoments}, data::Timematr, dat::Date;
     end
 end
 
+function fitModel(::Type{MovWinSampleMoments},
+                  data::Timematr,
+                  dat::Date; windSize::Int = 50)
+    ## extract data up to given date
+    validDates = idx(data) .<= dat
+
+    if sum(validDates) < windSize
+        return MovWinSampleMoments()
+    else
+        ## get data
+        rets = asArr(data, Float64)[validDates, :]
+
+        ## only take data equal with length equal to window size
+        rets = rets[(end-windSize+1):end, :]
+        
+        mus = mean(rets, 1)[:]
+        sigmas = cov(rets)
+        return MovWinSampleMoments(mus, sigmas, names(data))
+    end
+end
+
 function fitModel(::Type{ExpWeighted}, data::Timematr, dat::Date;
                   minObs::Int = 5, lambda = 0.96)
     if (lambda < 0) | (1 < lambda)
@@ -143,7 +195,7 @@ function fitModel(::Type{ExpWeighted}, data::Timematr, dat::Date;
         ## get data
         rets = asArr(data, Float64)[validDates, :]
 
-        nObs, nAss = size(data)
+        nObs, nAss = size(rets)
 
         ## get weights for observations
         obsWgts = lambda.^((nObs-1):(-1):0)
@@ -179,7 +231,7 @@ macro getTimeVaryingMomentsForMuSigmaModels(myType)
             
             for ii=1:length(allDats)
                 thisDat = allDats[ii]
-                
+
                 ## estimate moments
                 mod = AssetMgmt.fitModel(modType, data, thisDat)
                 
@@ -219,18 +271,9 @@ macro getTimeVaryingMomentsForMuSigmaModels(myType)
             return (musOverTimeTd, sigmasOverTimeTd, corrOverTimeTd)
         end
 
-        function hcat(inst::$(myType), inst2::$(myType))
-            ## check for equal indices
-            if idx(inst) != idx(inst2)
-                error("indices must coincide for hcat")
-            end
-            
-            instNew = $(myType)(hcat(inst.vals, inst2.vals), idx(inst))
-            return instNew
-        end
     end)
 end
 
-for t = (:(SampleMoments), :(ExpWeighted))
+for t = (:(SampleMoments), :(MovWinSampleMoments), :(ExpWeighted))
     eval(macroexpand(:(@getTimeVaryingMomentsForMuSigmaModels($t))))
 end
